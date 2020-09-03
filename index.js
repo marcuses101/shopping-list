e15f18b1e8da78d6cd0a3358146a2d72aab5c295
marcuses101 <mnjconnolly@gmail.com>
2020-09-02 21:40:43 -0400

    solve

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 //Run script on page load
 function handleSubmit(e) {
     e.preventDefault();
-    console.log($("input").val());
+    const listItem = `<li>
+                        <span class="shopping-item">${$("input").val()}</span>
+                        <div class="shopping-item-controls">
+                            <button class="shopping-item-toggle">
+                                 <span class="button-label">check</span>
+                             </button>
+                        <button class="shopping-item-delete">
+                            <span class="button-label">delete</span>
+                        </button>
+                        </div>
+                     </li>`
+    $("ul").append(listItem)
     this.reset();
 }
-function toggleCheck (){
 
+function toggleCheck() {
+    $(this).parents("li").children(".shopping-item").toggleClass("shopping-item__checked");
 }
 
-function deleteItem (){
-
+function deleteItem() {
+    $(this).parents('li').remove();
 }
 
-$(()=>{
-$("form").submit(handleSubmit)
+$(() => {
+    $("form").submit(handleSubmit);
+    $(".shopping-list").on("click", ".shopping-item-delete", deleteItem);
+    $(".shopping-list").on("click", ".shopping-item-toggle", toggleCheck);
 })
